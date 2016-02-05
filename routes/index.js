7f2f053e35d7f4406e38fea31423405f41fe1e32
Chance Snow <git@chancesnow.me>
2016-02-05 00:17:08 -0800

    Add more skeleton routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/history', function (req, res, next) {
+  // Get playback history (previously played songs)
+  res.send('respond with a resource');
+});
+
+router.get('/queue', function (req, res, next) {
+  // Get playback future
+  res.send('respond with a resource');
+});
+
+router.get('/search', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
