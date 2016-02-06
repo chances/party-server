bb104cca45ca216ae2dcd02fc613b742b9db0c64
Chance Snow <git@chancesnow.me>
2016-02-06 13:01:01 -0800

    Refactor CORS origin to be configurable

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var users = require('./routes/users');
 
 var app = express();
 
-// CORS with chancesnow.me
+// CORS with chancesnow.me or specific origin
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://chancesnow.me');
+  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || 'http://chancesnow.me');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
