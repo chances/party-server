c42195e9312743a98b32fcae3793834295dab183
Chance Snow <git@chancesnow.me>
2016-06-22 22:42:18 -0700

    Add placeholder test

---
+++ test/index.js
@@ -0,0 +1,8 @@
+var sinon = require('sinon');
+var expect = require('chai').expect;
+
+describe('Placeholder test', function () {
+  it('should assert true is true', function () {
+    expect(true).to.equal(true);
+  });
+});
