b924cb5cc31548f79e1e55ee159153019cb3984b
Chance Snow <git@chancesnow.me>
2016-07-03 16:29:45 -0700

    Really fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ $NODE_ENV == "production" ]; then
+if [ "$NODE_ENV" == "production" ]; then
   npm install babel-cli babel-preset-es2015-node6 babel-preset-stage-2
   npm run build
 fi
