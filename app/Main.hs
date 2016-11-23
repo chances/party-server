d31702f3b862d78d4599f29641be29c103b793bb
Chance Snow <git@chancesnow.me>
2016-11-23 00:35:33 -0800

    Load dotenv file for configuration

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import qualified Configuration.Dotenv     as Dotenv
 import           Network.Wai.Handler.Warp (run)
+import           System.Directory         (doesFileExist)
 import           System.IO                (BufferMode (LineBuffering), IO (..),
                                            hSetBuffering, stdout)
 
@@ -15,6 +17,18 @@ import           Models                   (doMigrations)
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
+
+    -- Load the .env configuration file, if present
+    dotenvExists <- doesFileExist ".env"
+    if dotenvExists
+        then Dotenv.loadFile False ".env"
+        else do
+            putStrLn "Warning: .env configuration file does not exist"
+            putStrLn
+                (
+                   "\tEnsure proper configuration exists in the environment"
+                ++ "(See .env.example)"
+                )
 
     -- Setup app configuration
     env <- lookupSetting "ENV" Development
