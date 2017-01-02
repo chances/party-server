222678c52bfeebae288ab9b588f5914a3f270e8a
Chance Snow <git@chancesnow.me>
2017-01-02 14:57:11 -0800

    Try the deploy script again
    https://gist.github.com/domenic/ec8b0fc8ab45f39403dd

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,50 +1,60 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-cd $TRAVIS_BUILD_DIR
+SOURCE_BRANCH=`git rev-parse --abbrev-ref HEAD`
+TARGET_BRANCH="staging"
 
-# Setup Heroku deployment tool
-HEROKU_DEPLOY_BINARY_VERSION="v0.2.1.0"
-HEROKU_DEPLOY_BINARY="heroku-deploy-binary-linux-x86_64"
+function doCompile {
+  ./bin/compile.sh
+}
 
-echo "Downloading heroku-deploy-binary..."
-
-mkdir dist/
-curl -L \
-  "https://github.com/joozek78/heroku-deploy-binary/releases/download/$HEROKU_DEPLOY_BINARY_VERSION/$HEROKU_DEPLOY_BINARY" \
-  > ./dist/$HEROKU_DEPLOY_BINARY
-chmod +x ./dist/$HEROKU_DEPLOY_BINARY
-
-echo "Done"
-echo ""
-
-# Pull requests shouldn't try to deploy
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "Skipping deploy on PRs"
+    echo "Skipping deploy in PR; just doing a build."
+    doCompile
     exit 0
 fi
 
-echo "Installing chances-party-exe to ./dist"
-echo ""
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
 
-# Install the chances-party executable for deployment
-stack --local-bin-path dist/ install # TODO: Add optimization flags
+# Add deploy key to ssh agent to push to GitHub
+openssl aes-256-cbc -K $encrypted_5894ebfdb1ab_key -iv $encrypted_5894ebfdb1ab_iv \
+  -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
-echo "Done"
-echo ""
-echo "Deploying chances-party-exe to Heroku..."
-
+# Clone the existing gh-pages for this repo into dist/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
+git clone $REPO dist
 cd dist
-sh $HEROKU_DEPLOY_BINARY chances-party chances-party-exe -t $HEROKU_KEY
-
-echo ""
-echo "Successfully deployed!"
-echo ""
-
-echo "Cleaning up Heroku deployment tool"
-rm $HEROKU_DEPLOY_BINARY
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-echo ""
-echo "Done"
-echo ""
+# Clean existing contents from dist
+rm -rf dist/**/* || exit 0
+
+# Run our compile script
+doCompile
+
+# Now let's go have some fun with the cloned repo
+cd dist
+git config user.name "Travis CI"
+git config user.email "git@chancesnow.me"
+
+# If there are no changes to the compiled dist (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add .
+git commit -m "Deploy to staging: ${SHA}"
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
