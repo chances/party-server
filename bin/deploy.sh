936b8b55754d220f35dc5f37fe204516cb6c051d
Chance Snow <git@chancesnow.me>
2017-01-02 10:37:28 -0800

    Fix deploy script and add tracing messages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-# Setup Heroku binary deployment tool
+cd $TRAVIS_BUILD_DIR
+
+# Setup Heroku deployment tool
 HEROKU_DEPLOY_BINARY_VERSION="v0.2.1.0"
 HEROKU_DEPLOY_BINARY="heroku-deploy-binary-linux-x86_64"
 
+echo "Downloading heroku-deploy-binary..."
+
 mkdir dist/
-travis_retry curl -L \
-https://github.com/joozek78/heroku-deploy-binary/releases/download/$HEROKU_DEPLOY_BINARY_VERSION/$HEROKU_DEPLOY_BINARY
-mv $HEROKU_DEPLOY_BINARY dist/$HEROKU_DEPLOY_BINARY
+curl -L \
+  "https://github.com/joozek78/heroku-deploy-binary/releases/download/$HEROKU_DEPLOY_BINARY_VERSION/$HEROKU_DEPLOY_BINARY" \
+  > ./dist/$HEROKU_DEPLOY_BINARY
+chmod +x ./dist/$HEROKU_DEPLOY_BINARY
+
+echo "Done"
+echo ""
 
 # Pull requests shouldn't try to deploy
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
@@ -16,17 +24,27 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     exit 0
 fi
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
-doInstall
+echo "Installing chances-party-exe to ./dist"
+echo ""
 
 # Install the chances-party executable for deployment
 stack --local-bin-path dist/ install # TODO: Add optimization flags
+
+echo "Done"
+echo ""
+echo "Deploying chances-party-exe to Heroku..."
+
 cd dist
 sh $HEROKU_DEPLOY_BINARY chances-party chances-party-exe -t $HEROKU_KEY
+
+echo ""
+echo "Successfully deployed!"
+echo ""
+
+echo "Cleaning up Heroku deployment tool"
+rm $HEROKU_DEPLOY_BINARY
+cd ..
+
+echo ""
+echo "Done"
+echo ""
