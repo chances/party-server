120de065c9dc4e9cb4e295a2558e2ba44f37b86b
Chance Snow <git@chancesnow.me>
2017-04-02 07:57:27 -0700

    Add Cors and static files middleware

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gopkg.in/gin-contrib/cors.v1"
 )
 
 func main() {
@@ -14,11 +19,46 @@ func main() {
 		log.Println("Warning: .env file is not present. Using system provided environment variables")
 	}
 
-	router := gin.Default()
+	g := gin.New()
+	g.Use(gin.Logger())
 
-	router.GET("/", func(c *gin.Context) {
+	// === Middleware ===
+	// CORS
+	corsOrigins :=
+		strings.Split(getenv("CORS_ORIGINS", "https://chancesnow.me"), ",")
+	g.Use(cors.New(cors.Config{
+		AllowOrigins:     corsOrigins,
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	// Static files
+	g.Static("/css/", "./public")
+
+	g.Use(gin.Recovery())
+
+	// Application routes
+	g.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, world!")
 	})
 
-	router.Run()
+	g.Run()
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
+func getenvOrFail(key string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		fmt.Fprintf(os.Stderr, "error: Missing environment variable: %v\n", key)
+		os.Exit(1)
+	}
+	return value
 }
