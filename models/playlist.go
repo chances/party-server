8ee546fae1fc57fa17551b12aa50e5b8933a5e9d
Chance Snow <git@chancesnow.me>
2017-05-24 22:36:37 -0700

    Add Playlist models

---
+++ models/playlist.go
@@ -0,0 +1,30 @@
+package models
+
+import "github.com/zmb3/spotify"
+
+type Playlist struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Owner       string `json:"owner"`
+	Endpoint    string `json:"endpoint"`
+	TotalTracks uint   `json:"total_tracks"`
+}
+
+type CachedPlaylist struct {
+  Playlist Playlist `json:"playlist"`
+  Tracks []Track
+}
+
+func NewPlaylist(p spotify.SimplePlaylist) Playlist {
+	return Playlist{
+		ID:          p.ID.String(),
+		Name:        p.Name,
+		Owner:       p.Owner.ID,
+		Endpoint:    p.ExternalURLs["spotify"],
+		TotalTracks: p.Tracks.Total,
+	}
+}
+
+type Playlists struct {
+	Playlists []Playlist
+}
