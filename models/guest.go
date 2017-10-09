c8d333ab32d43a37cbf888f79e0e445c60806905
Chance Snow <git@chancesnow.me>
2017-10-09 00:50:57 -0700

    Add Party guests helpers

--- models/guest.go
+++ models/guest.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"database/sql"
+	"encoding/json"
+
+	"github.com/vattle/sqlboiler/types"
+)
+
 type Guest struct {
 	Name      string `json:"name"`
 	Alias     string `json:"alias"`
@@ -12,4 +19,29 @@ func NewGuest(name string) Guest {
 		Alias:     "",
 		CheckedIn: false,
 	}
+}
+
+func (o *Party) Guests() ([]Guest, error) {
+	guestList, err := o.GuestG().One()
+	if err != nil {
+		if err != sql.ErrNoRows {
+			return make([]Guest, 0), err
+		}
+	}
+	var guests []Guest
+	err = guestList.Data.Unmarshal(&guests)
+	if err != nil {
+		return make([]Guest, 0), err
+	}
+	return guests, nil
+}
+
+func (o *Party) UpdateGuestList(guests []Guest) error {
+	guestsJsonRaw, _ := json.Marshal(guests)
+	guestsJson := types.JSON(guestsJsonRaw)
+	guestList := GuestList{
+		ID:   o.GuestsID,
+		Data: guestsJson,
+	}
+	return guestList.UpdateG()
 }
