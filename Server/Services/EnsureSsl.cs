e98ef75bb26098acb10bd0e5a4d1bfa14d1ed4fd
Chance Snow <git@chancesnow.me>
2018-07-16 04:58:11 -0700

    Add EnsureSsl automagic pipeline

---
+++ Server/Services/EnsureSsl.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Configuration;
+using Nancy.Responses;
+using Server.Configuration;
+
+namespace Server.Services
+{
+  [UsedImplicitly]
+  public class EnsureSsl : IRequestStartup
+  {
+    private readonly Mode _mode;
+
+    public EnsureSsl(INancyEnvironment environment)
+    {
+      _mode = environment.GetValue<Mode>();
+    }
+
+    public void Initialize(IPipelines pipelines, NancyContext context)
+    {
+      pipelines.BeforeRequest.AddItemToStartOfPipeline(RedirectForSsl);
+    }
+
+    private Response RedirectForSsl(NancyContext context)
+    {
+      var url = context.Request.Url;
+      if (_mode.IsDevelopment() || url.IsSecure) return context.GetResponse();
+
+      url.Scheme = "https";
+      return new RedirectResponse(url.ToString());
+    }
+  }
+}
