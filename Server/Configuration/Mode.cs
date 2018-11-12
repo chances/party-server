a9de36bd1dca70fe10166d9e6a0797443b8be70e
Chance Snow <git@chancesnow.me>
2018-11-11 23:34:01 -0800

    Add AppConfig class

---
+++ Server/Configuration/Mode.cs
@@ -0,0 +1,21 @@
+﻿namespace Server.Configuration
+{
+  public enum Mode
+  {
+    Development,
+    Production
+  }
+
+  public static class ModeExtensions
+  {
+    public static bool IsProduction(this Mode mode)
+    {
+      return mode == Mode.Production;
+    }
+
+    public static bool IsDevelopment(this Mode mode)
+    {
+      return mode == Mode.Development;
+    }
+  }
+}
