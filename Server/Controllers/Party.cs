db53439ee018f716cc715a7c3773969a7c80aa0b
Chance Snow <git@chancesnow.me>
2019-01-20 18:14:38 -0800

    Add Start action to Party controller and integrate with admin portal

--- Server/Controllers/Party.cs
+++ Server/Controllers/Party.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Db = Models;
 using Server.Data;
 using Server.Models;
 using Server.Services;
+using Newtonsoft.Json;
 
 namespace Server.Controllers
 {
+  [Route("/party")]
   public class Party : Controller
   {
     private RoomCodeGenerator _roomCodeGenerator;
     private UserProvider _userProvider;
+    private readonly SpotifyRepository _spotify;
+    private readonly Db.PartyModelContainer _db;
 
     public Party(
       RoomCodeGenerator roomCodeGenerator,
-      UserProvider userProvider
+      UserProvider userProvider,
+      SpotifyRepository spotify,
+      Db.PartyModelContainer db
     )
     {
       _roomCodeGenerator = roomCodeGenerator;
       _userProvider = userProvider;
+      _spotify = spotify;
+      _db = db;
     }
 
     [HttpGet]
-    [Route("/party")]
+    [Route("")]
     public async Task<IActionResult> Index()
     {
       var user = await _userProvider.GetUserAsync();
@@ -37,6 +46,79 @@ namespace Server.Controllers
         party.Id.ToString(),
         PublicParty.FromParty(party)
       ));
+    }
+
+    [HttpPost]
+    [Route("start")]
+    public async Task<IActionResult> Start([FromBody] NewResourceDocument<NewParty> newParty)
+    {
+      if (!ModelState.IsValid) return Error.BadRequest(ModelState.Errors());
+
+      var playlistId = newParty.Data.Attributes.PlaylistId;
+
+      var user = await _userProvider.GetUserAsync(_db);
+      if (user == null) return Unauthorized();
+
+      // End the user's current party if they've already started one
+      var currentParty = user.Party;
+      if (currentParty != null)
+      {
+        // TODO: Call end party action?
+      }
+
+      var playlists = await _spotify.GetMyPlaylists();
+      var currentPlaylist = playlists.FirstOrDefault(p => p.Id == playlistId);
+
+      // If the Playlist doesn't belong to user, bad request
+      if (currentPlaylist == null)
+      {
+        return Error.BadRequest(
+          $"Invalid playlist {playlistId}. User '{user.Username}' does not own or subscribe to given playlist"
+        );
+      }
+
+      string roomCode = _roomCodeGenerator.NewRoomCode;
+      if (string.IsNullOrWhiteSpace(roomCode))
+      {
+        return Error.Internal("Could not generate room code");
+      }
+
+      var location = newParty.Data.Attributes.Location;
+
+      var party = new Db.Party
+      {
+        RoomCode = roomCode,
+        Location = JsonConvert.SerializeObject(location)
+      };
+      user.Party = party;
+      user.UpdatedAt = DateTime.UtcNow;
+
+      var playlist = await _spotify
+        .GetPlaylistAsync(user.Username, currentPlaylist.Id);
+
+      // New party's queue
+      party.Queue = new Db.TrackList
+      {
+        Data = JsonConvert.SerializeObject(playlist.Tracks)
+      };
+      _db.TrackList.Add(party.Queue);
+
+      // New party's playback history
+      party.History = new Db.TrackList
+      {
+        Data = JsonConvert.SerializeObject(new List<object>(0))
+      };
+      _db.TrackList.Add(party.History);
+
+      // New party's guest list
+      party.Guests = new Db.GuestList
+      {
+        Data = JsonConvert.SerializeObject(new List<object>(0))
+      };
+
+      await _db.SaveChangesAsync();
+
+      return Ok(Document.ResourceIdentifier<Party>(roomCode));
     }
   }
 }
--- Server/Services/SpotifyRepository.cs
+++ Server/Services/SpotifyRepository.cs
@@ -8,9 +8,11 @@ using System.Linq;
 using System.Security.Claims;
 using Server.Services.Authentication;
 using Microsoft.Extensions.Caching.Distributed;
+using Models;
 using Newtonsoft.Json;
 using Server.Models;
 using Server.Services.Background;
+using Microsoft.EntityFrameworkCore;
 
 namespace Server.Services
 {
@@ -19,24 +21,36 @@ namespace Server.Services
     private static readonly IEnumerable<Playlist> EmptyList = new List<Playlist>(0);
 
     private readonly IBackgroundTaskQueue _background;
+    private readonly PartyModelContainer _db;
     private readonly IDistributedCache _cache;
-    private readonly Token _spotifyAccessToken;
+    private readonly SpotifyWebAPI _api;
 
     public SpotifyRepository(
-      IBackgroundTaskQueue queue,
+      IBackgroundTaskQueue bgQueue,
+      PartyModelContainer db,
       IDistributedCache cache,
       IHttpContextAccessor context
     ) : base(context)
     {
-      _background = queue;
+      _background = bgQueue;
+      _db = db;
       _cache = cache;
 
       if (HttpContext.User.IsInRole(Roles.Host))
       {
-        _spotifyAccessToken = SpotifyAuthenticationScheme.GetToken(HttpContext.User.Claims);
+        var token = SpotifyAuthenticationScheme.GetToken(HttpContext.User.Claims);
+        if (token == null) return;
+
+        _api = new SpotifyWebAPI()
+        {
+          UseAuth = true,
+          AccessToken = token.AccessToken,
+          TokenType = token.TokenType
+        };
       }
     }
 
+    // TODO: Rename these with async suffixes
     public async Task<IEnumerable<Playlist>> GetMyOwnPlaylists()
     {
       var user = HttpContext.User;
@@ -60,20 +74,13 @@ namespace Server.Services
 
       var userId = user.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
 
-      if (_spotifyAccessToken == null) return EmptyList;
+      if (_api == null) return EmptyList;
 
       var playlists = await _cache.GetOrDeferAsync(
         $"playlists:{userId}",
         JsonConvert.DeserializeObject<List<Playlist>>,
         async () =>
         {
-          var api = new SpotifyWebAPI()
-          {
-            UseAuth = true,
-            AccessToken = _spotifyAccessToken.AccessToken,
-            TokenType = _spotifyAccessToken.TokenType
-          };
-
           var results = new List<Playlist>();
 
           Paging<SimplePlaylist> page = null;
@@ -82,7 +89,7 @@ namespace Server.Services
             var offset = page != null
               ? page.Offset + page.Items.Count
               : 0;
-            page = await api.GetUserPlaylistsAsync(userId, 50, offset);
+            page = await _api.GetUserPlaylistsAsync(userId, 50, offset);
 
             IEnumerable<SimplePlaylist> pageOfPlaylists = page.Items;
 
@@ -109,6 +116,99 @@ namespace Server.Services
       });
 
       return playlists;
+    }
+
+    public async Task<CachedPlaylist> GetPlaylistAsync(string userId, string playlistId)
+    {
+      // Try get the cached playlist from the cache, database, or via Spotify
+      //  on cache & DB miss
+      return await _cache.GetOrDeferAsync($"playlist:{playlistId}", async () =>
+      {
+        var trackList = await _db.TrackList
+          .FirstOrDefaultAsync(t => t.SpotifyPlaylistId == playlistId);
+
+        var oneHour = TimeSpan.FromHours(1);
+        var oneDay = TimeSpan.FromHours(24);
+
+        // Only cache playlists in the DB for one day
+        var dbCacheValidWindow = DateTime.UtcNow
+          .Add(oneDay * -1);
+        var dbCacheEntryValid = trackList != null &&
+          trackList.UpdatedAt >= dbCacheValidWindow;
+
+        if (dbCacheEntryValid)
+        {
+          return JsonConvert.DeserializeObject<CachedPlaylist>(
+            trackList.Data
+          );
+        }
+
+        // Both caches missed, get playlist from Spotify
+        var playlist = await _api.GetPlaylistAsync(userId, playlistId);
+        var tracks = playlist.Tracks.Items
+          .Where(t => !t.IsLocal)
+          .Select(t => Track.FromSpotify(t.Track))
+          .ToList();
+
+        // Get the other pages of tracks
+        var page = playlist.Tracks;
+        while (page.HasNextPage())
+        {
+          page = await _api.GetPlaylistTracksAsync(
+            userId,
+            playlistId,
+            offset: page.Offset + page.Limit
+          );
+          tracks.AddRange(page.Items
+            .Where(t => !t.IsLocal)
+            .Select(t => Track.FromSpotify(t.Track))
+          );
+        }
+
+        var cachedPlaylist = new CachedPlaylist
+        {
+          Playlist = Playlist.FromSpotify(playlist),
+          Tracks = tracks
+        };
+
+        // Cache the playlist in the default cache
+        _background.QueueTask(async token =>
+        {
+          await _cache.SetAsync(
+            $"playlist:{playlistId}",
+            cachedPlaylist,
+            new DistributedCacheEntryOptions
+            {
+              SlidingExpiration = oneHour
+            }
+          );
+        });
+
+        // Cache the playlist in the DB
+        _background.QueueTask(async token =>
+        {
+          var playlistJson = JsonConvert.SerializeObject(cachedPlaylist);
+
+          if (trackList == null)
+          {
+            trackList = new TrackList
+            {
+              SpotifyPlaylistId = playlistId,
+              Data = playlistJson
+            };
+            _db.Add(trackList);
+          }
+          else
+          {
+            trackList.Data = playlistJson;
+            trackList.UpdatedAt = DateTime.UtcNow;
+          }
+
+          await _db.SaveChangesAsync();
+        });
+
+        return cachedPlaylist;
+      });
     }
   }
 }
