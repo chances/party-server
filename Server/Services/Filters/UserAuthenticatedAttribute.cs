fdc9bca146c0f24f1ffa8a41f348ed1e375fe10d
Chance Snow <git@chancesnow.me>
2019-01-26 00:14:16 -0800

    Add www-authenticate header to UserAuthenticated filter response

--- Server/Services/Filters/UserAuthenticatedAttribute.cs
+++ Server/Services/Filters/UserAuthenticatedAttribute.cs
@@ -1,10 +1,14 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Net.Http.Headers;
 using Server.Services.Authentication;
 
 namespace Server.Services.Filters
 {
+  /// <summary>
+  /// Filter requests to a Party API endpoint to those who are authenticated.
+  /// </summary>
   [AttributeUsage(AttributeTargets.Method)]
   public class UserAuthenticatedAttribute : ActionFilterAttribute
   {
@@ -13,10 +17,13 @@ namespace Server.Services.Filters
       var isHostUser = context.HttpContext.User?.IsInRole(Roles.Host) ?? false;
       var isGuestUser = context.HttpContext.User?.IsInRole(Roles.Guest) ?? false;
       var authenticated = isHostUser || isGuestUser;
-      if (!authenticated)
-      {
-        context.Result = new UnauthorizedResult();
-      }
+      if (authenticated) return;
+
+      context.Result = new UnauthorizedResult();
+      context.HttpContext.Response.Headers.Add(
+        HeaderNames.WWWAuthenticate,
+        "Bearer realm=\"spotify\", error=\"unauthorized\", error_description=\"Unauthorized request made to Party\""
+      );
     }
   }
 }
