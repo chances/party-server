5a135b4e4bc1db4613b4b4515688222560b016ec
Chance Snow <git@chancesnow.me>
2019-09-14 15:40:26 -0700

    Improve Postgres connection string converter related to SSL Mode

--- Models/ConnectionStringExtensions.cs
+++ Models/ConnectionStringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Web;
 
 namespace Models
 {
@@ -9,16 +10,19 @@ namespace Models
     {
       var isUrl = Uri.TryCreate(source, UriKind.Absolute, out var url);
       if (!isUrl) throw new ArgumentException($"Invalid PgSQL connection string: {source}", nameof(source));
-      var queryParams = url.Query.Split(';').ToDictionary(
-        s => s.Contains('=') ? s.Split('=')[0].Trim() : s,
-        s => s.Contains('=') ? s.Split('=')[1].Trim() : ""
-      );
-      var requireSsl = queryParams.ContainsKey("ssl") && queryParams["ssl"] == "true";
+      var port = url.Port == -1 ? 5432 : url.Port;
+      var queryParams = HttpUtility.ParseQueryString(url.Query);
+      var requireSsl = queryParams["ssl"] != null && queryParams["ssl"] == "true";
+      var disableSsl = queryParams["ssl"] != null && queryParams["ssl"] == "false";
       var connectionString =
-        $"Host={url.Host};Port={url.Port};Database={url.LocalPath.Substring(1)};Username={url.UserInfo.Split(':')[0]};Password={url.UserInfo.Split(':')[1]};SSL Mode=Prefer;Pooling=true;";
+        $"Host={url.Host};Port={port};Database={url.LocalPath.Substring(1)};Username={url.UserInfo.Split(':')[0]};Password={url.UserInfo.Split(':')[1]};SSL Mode=Prefer;Pooling=true;";
       if (requireSsl)
       {
         connectionString = connectionString.Replace("SSL Mode=Prefer", "SSL Mode=Require");
+      }
+      if (disableSsl)
+      {
+        connectionString = connectionString.Replace("SSL Mode=Prefer", "SSL Mode=Disable");
       }
 
       if (!isDevelopmentEnv)
